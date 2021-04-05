7fb8ca7015588a3570e5c51f7390661691a517ca
laloehm <laloehm1@gmail.com>
2021-04-04 20:53:20 -0500

    Mas ajustes en modo responsive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$('.owl-carousel').owlCarousel({
+$(".owl-carousel").owlCarousel({
     loop: true,
     margin: 10,
     autoplay: true,
@@ -8,40 +8,55 @@ $('.owl-carousel').owlCarousel({
         0: {
             items: 1,
             nav: true,
-            dots: false
+            dots: false,
         },
         767: {
             items: 3,
             nav: true,
-            dots: false
+            dots: false,
         },
         1000: {
             items: 3,
             nav: true,
-            dots: false
-        }
-    }
-})
+            dots: false,
+        },
+    },
+});
 /* Smooth scrolling para anclas */
 $("a.smooth").on("click", function (e) {
     e.preventDefault();
     var $link = $(this);
     var anchor = $link.attr("href");
-    $("html, body").stop().animate({
-        scrollTop: $(anchor).offset().top - 100 + "px"
-    }, 1000);
+    $("html, body")
+        .stop()
+        .animate(
+            {
+                scrollTop: $(anchor).offset().top - 100 + "px",
+            },
+            1000
+        );
 });
 
 // Funcion para barra de Menu
 $(window).scroll(function () {
     let header = $("#menu");
     if ($("#menu").offset().top > 300) {
-        $(header).css('background', '#fff');
+        $(header).css("background", "#fff");
     } else {
-        $(header).css('background', 'transparent');        
+        $(header).css("background", "transparent");
     }
 });
-$('body').scrollspy({target: ".navbar", offset: 170});
+$("body").scrollspy({ target: ".navbar", offset: 170 });
 
+(function ($) {
+    "use strict";
+    $('[data-icon="hamburguer"]').on("click", function () {
+        if (!$('[data-icon="hamburguer-bar"]').hasClass("bar-on")) {
+            $('[data-icon="hamburguer-bar"]').addClass("bar-on");
+        } else {
+            $('[data-icon="hamburguer-bar"]').removeClass("bar-on");
+        }
+    });
+})(jQuery);
 
 /* git remote add origin https://github.com/laloehm/baby_bamboo.git */
